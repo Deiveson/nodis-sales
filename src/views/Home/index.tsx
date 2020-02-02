24449fe9d7a8bd035a2defa83d39dd9c842a791f
Deiveson <deivesontec@gmail.com>
2020-02-01 22:19:36 -0300

    botão no banner

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logoAlt from '../../assets/images/logo-white.png';
+import Button from '../../components/button';
 
 const Home: React.FC = () => (
   <section className="home">
     <section className="home__banner">
       <div className="home__banner--text">
         Produtos de marketplace em lojas físicas!?
+        <div className="home__banner--button">
+          <Button color="white" text="Saiba Mais" />
+        </div>
       </div>
       <div className="home__banner--logo"><img src={logoAlt} alt="logo-alt" /></div>
     </section>
