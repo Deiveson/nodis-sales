8150f121ebdb7421b5e5927bb98354da5e348ec2
Deiveson <deivesontec@gmail.com>
2020-02-01 23:12:26 -0300

    titulo da área de produtos

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import logoAlt from '../../assets/images/logo-white.png';
 import Button from '../../components/button';
 
+const Banner: React.FC = () => (
+  <section className="home__banner">
+    <div className="home__banner--text">
+            Produtos de marketplace em lojas físicas!?
+      <div className="home__banner--button">
+        <Button color="white" text="Saiba Mais" />
+      </div>
+    </div>
+    <div className="home__banner--logo"><img src={logoAlt} alt="logo-alt" /></div>
+  </section>
+);
+
 const Home: React.FC = () => (
   <section className="home">
-    <section className="home__banner">
-      <div className="home__banner--text">
-        Produtos de marketplace em lojas físicas!?
-        <div className="home__banner--button">
-          <Button color="white" text="Saiba Mais" />
-        </div>
+    <Banner />
+    <section className="home__products">
+      <div className="home__products__header">
+        <div className="home__products__header--title title">Ofertas Exclusivas</div>
+        <div className="home__products__header--aside ">ver todos</div>
       </div>
-      <div className="home__banner--logo"><img src={logoAlt} alt="logo-alt" /></div>
+      <div className="home__products__container">
+      </div>
     </section>
   </section>
 );
