94de5d8d19c0052c1d6fcdf5cbc902d8fdc10db5
Deiveson <deivesontec@gmail.com>
2020-02-02 10:42:55 -0300

    rotas, loafding e ajustes no component product-card

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import history from '../../history';
 import { formatCurrency } from '../util/fnUtils';
 
-const ProductCard: React.FC = () => (
-  <div className="product-card">
-    <div className="product-card__image">
-      <img src="https://images-shoptime.b2w.io/produtos/01/00/sku/7134/2/7134233P.jpg" alt="panela" />
+interface Props {
+    id: number;
+    name: string;
+    imageUrl: string;
+    salePrice: string;
+    promotionalPrice: string;
+}
+
+const ProductCard: React.FC<Props> = (props) => {
+  const {
+    name = '', imageUrl = '', salePrice = '', promotionalPrice = '', id = null,
+  } = props;
+  return (
+    <div className="product-card" tabIndex={1} onClick={() => history.push(`/produto/${id}`)}>
+      <div className="product-card__image">
+        <img src={imageUrl} alt="panela" />
+      </div>
+      <div className="product-card__name">{name}</div>
+      <div className="product-card__price">
+        <span>
+          R$
+          {' '}
+          {useMemo(() => formatCurrency(salePrice), [salePrice])}
+        </span>
+        <span className="product-card__price--promotional">
+          R$
+          {' '}
+          {useMemo(() => formatCurrency(promotionalPrice), [promotionalPrice])}
+        </span>
+      </div>
     </div>
-    <div className="product-card__name">Panela de Arroz 10 xícaras ML-2900 NKS Milano 220V</div>
-    <div className="product-card__price">
-      <span>
-        R$
-        {' '}
-        {formatCurrency('8400')}
-      </span>
-      <span className="product-card__price--promotional">
-        R$
-        {' '}
-        {formatCurrency('9000')}
-      </span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ProductCard;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoAlt from '../../assets/images/logo-white.png';
 import Button from '../../components/button';
 import ProductCard from '../../components/product-card';
 
+import loding from '../../assets/images/loading-bounce.svg';
+
+interface Product {
+    id: number;
+    name: string;
+    imageUrl: string;
+    salePrice: string;
+    promotionalPrice: string;
+}
 const Banner: React.FC = () => (
   <section className="home__banner">
     <div className="home__banner--text">
-            Produtos de marketplace em lojas físicas!?
+      Produtos de marketplace em lojas físicas!?
       <div className="home__banner--button">
         <Button color="white" text="Saiba Mais" />
       </div>
@@ -15,20 +24,41 @@ const Banner: React.FC = () => (
   </section>
 );
 
-const Home: React.FC = () => (
-  <section className="home">
-    <Banner />
-    <section className="home__products">
-      <div className="home__products__header">
-        <div className="home__products__header--title title">Ofertas Exclusivas</div>
-        <div className="home__products__header--aside ">ver todos</div>
-      </div>
-      <div className="home__products__container">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+const Home: React.FC = () => {
+  const [products, setProducts] = useState<[Product]>();
+  useEffect(() => {
+    loadProducts();
+  }, []);
+  async function loadProducts() {
+    const response = await fetch('https://frontend-challenge-beginner.herokuapp.com/skus/');
+    const data = await response.json();
+    setProducts(data);
+  }
+
+  return (
+    <section className="home">
+      <Banner />
+      <section className="home__products">
+        <div className="home__products__header">
+          <div className="home__products__header--title title">Ofertas Exclusivas</div>
+          <div className="home__products__header--aside ">ver todos</div>
+        </div>
+        {products ? (
+          <div className="home__products__container">
+            {products?.map((product) => (
+              <ProductCard
+                promotionalPrice={product.promotionalPrice}
+                salePrice={product.salePrice}
+                imageUrl={product.imageUrl}
+                name={product.name}
+                id={product.id}
+                key={product.id}
+              />
+            ))}
+          </div>
+        ) : <div style={{ textAlign: 'center' }}><img src={loding} alt="loading..." /></div>}
+      </section>
     </section>
-  </section>
-);
+  );
+};
 export default Home;
